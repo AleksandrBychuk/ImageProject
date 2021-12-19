13fc9a239d87f11833ecc596e68338c763e8c84c
AleksandrBychuk <sbychuk645@gmail.com>
2021-12-19 17:44:41 +0300

    new method check greenpercent

--- ImageProject/Controllers/HomeController.cs
+++ ImageProject/Controllers/HomeController.cs
@@ -120,7 +120,7 @@ namespace ImageProject.Controllers
                 List<float> hex = new();
                 MCvTermCriteria term = new MCvTermCriteria(100, 0.5);
                 term.Type = TermCritType.Iter | TermCritType.Eps;
-
+                int bc = 0, rc = 0, gc = 0;
                 for (int y = 0; y < src.Rows; y++)
                 {
                     for (int x = 0; x < src.Cols; x++)
@@ -128,22 +128,28 @@ namespace ImageProject.Controllers
                         samples.Data[y + x * src.Rows, 0] = (float)src[y, x].Blue;
                         samples.Data[y + x * src.Rows, 1] = (float)src[y, x].Green;
                         samples.Data[y + x * src.Rows, 2] = (float)src[y, x].Red;
-                        //if ((float)src[y, x].Green > (float)src[y, x].Blue && (float)src[y, x].Green > (float)src[y, x].Red)
-                        //    greenPercent += 1f / ((float)src.Width * (float)src.Height) * 100f;
+                        if ((float)src[y, x].Green + 1f >= (float)src[y, x].Blue && (float)src[y, x].Green + 1f >= (float)src[y, x].Red)
+                            greenPercent += 1f / ((float)src.Width * (float)src.Height) * 100f;
+                        if ((float)src[y, x].Green < (float)src[y, x].Blue && (float)src[y, x].Green < (float)src[y, x].Red)
+                            gc++;
+                        if ((float)src[y, x].Green < (float)src[y, x].Blue)
+                            bc++;
+                        if ((float)src[y, x].Green < (float)src[y, x].Red)
+                            rc++;
                         //float result = (2f * ((float)src[y, x].Green - (float)src[y, x].Red - (float)src[y, x].Blue)) / (2f * ((float)src[y, x].Green + (float)src[y, x].Red + (float)src[y, x].Blue));
                         //float result = (2f * (float)src[y, x].Green - (float)src[y, x].Red - (float)src[y, x].Blue) / (2f * (float)src[y, x].Green + (float)src[y, x].Red + (float)src[y, x].Blue);
                         //greenPercent += ((float)Math.Pow((float)src[y, x].Green, 2) - (float)Math.Pow((float)src[y, x].Red, 2)) / ((float)Math.Pow((float)src[y, x].Green, 2) + (float)Math.Pow((float)src[y, x].Red, 2));
-                        float R = (float)src[y, x].Red;
-                        float G = (float)src[y, x].Green;
-                        float B = (float)src[y, x].Blue;
-                        //float vari = (G - R) / (G + R - B);
-                        float grvi = (G - R) / (G + R);
-                        //float mgrvi = ((float)Math.Pow(G,2) - (float)Math.Pow(R, 2)) / ((float)Math.Pow(G, 2) + (float)Math.Pow(R, 2));
-                        float rgbvi = ((float)Math.Pow(G, 2) - B * R) / ((float)Math.Pow(G, 2) + B * R);
-                        //float cive = 0.441f * R - 0.881f * G + 0.385f * B + 18.7875f; // такое себе решение
-                        float gla = (2f * (float)src[y, x].Green - (float)src[y, x].Red - (float)src[y, x].Blue) / (2f * (float)src[y, x].Green + (float)src[y, x].Red + (float)src[y, x].Blue);
-                        if (gla > 0f)
-                            greenPercent += grvi;
+                        //float R = (float)src[y, x].Red;
+                        //float G = (float)src[y, x].Green;
+                        //float B = (float)src[y, x].Blue;
+                        ////float vari = (G - R) / (G + R - B);
+                        //float grvi = (G - R) / (G + R);
+                        ////float mgrvi = ((float)Math.Pow(G,2) - (float)Math.Pow(R, 2)) / ((float)Math.Pow(G, 2) + (float)Math.Pow(R, 2));
+                        //float rgbvi = ((float)Math.Pow(G, 2) - B * R) / ((float)Math.Pow(G, 2) + B * R);
+                        ////float cive = 0.441f * R - 0.881f * G + 0.385f * B + 18.7875f; // такое себе решение
+                        //float gla = (2f * (float)src[y, x].Green - (float)src[y, x].Red - (float)src[y, x].Blue) / (2f * (float)src[y, x].Green + (float)src[y, x].Red + (float)src[y, x].Blue);
+                        //if (gla > 0f)
+                        //    greenPercent += grvi;
                         //float R = (float)src[y, x].Red;
                         //float G = (float)src[y, x].Green;
                         //float B = (float)src[y, x].Blue;
